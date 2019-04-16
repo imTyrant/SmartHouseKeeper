edc6726466dbd48cd3f2007a8771c12c13c7eee9
imTyrant <cd2235823998@gmail.com>
2019-04-16 23:18:12 +0800

    fix something

--- app/main.js
+++ app/main.js
@@ -1,24 +1,8 @@
-const os = require('os');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const Menu = electron.Menu;
-const dialog = electron.dialog;
-const ipcMain = electron.ipcMain;
 
-ipcMain.on('pick-file-path', (event) => {
-    var option = {
-        title: 'log',
-        filter: [
-            {name: 'txt', extensions:['txt']}
-        ]
-    };
-    dialog.showSaveDialog(option, (file) => {
-        if (file) {
-            event.sender.send('file-path-selected', file);
-        }
-    })
-});
+var windows = null;
 
 function createWindow() {
     mainWin = new BrowserWindow({
@@ -27,18 +11,11 @@ function createWindow() {
         nodeIntegration: true
     });
 
-    mainWin.loadFile('index.html');
+    mainWin.loadFile('index.html')
 
     mainWin.on('closed', () => {
         mainWin = null
-    });
-}
-
-function quit() {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+    })
 }
 
 app.on('ready', createWindow);
-app.on("window-all-closed", quit);
