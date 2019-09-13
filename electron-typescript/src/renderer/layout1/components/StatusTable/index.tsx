347bc9a95018b1788766861b28a6e0574ff3d9a4
imTyrant <cd2235823998@gmail.com>
2019-09-14 00:32:58 +0800

    modify StatusTable component

--- electron-typescript/src/renderer/layout1/components/StatusTable/index.tsx
+++ electron-typescript/src/renderer/layout1/components/StatusTable/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export interface IStatusTableProps {
+export interface StatueTablePanel {
+    room: string;
+    devices: Array<string> | [];
+}
 
+export interface IStatusTableProps {
+    panels: Array<StatueTablePanel>;
 };
 
 export interface IStatusTableState {
@@ -9,11 +14,36 @@ export interface IStatusTableState {
 };
 
 export default class StatueTable extends React.Component<IStatusTableProps, IStatusTableState> {
+
     constructor(props) {
         super(props)
     }
 
     render() {
-        return (<div/>);
+        let a: Array<StatueTablePanel>;
+        a.map((value, index) => {
+            <Panel header={value.room} key={`${index}`}>
+                <p>{text}</p>
+            </Panel>
+        })
+        return (
+        <div>
+            <Collapse defaultActiveKey={['1']}>
+                {
+                    
+
+                    this.props.panels.map((value, index) => {
+                        <Panel header={value.room} key={`${index}`}>
+                            {
+                                value.devices.map((value, index) => {
+                                    <span>{value}</span>
+                                })
+                            }
+                        </Panel>
+                    })
+                }
+            </Collapse>
+        </div>
+        );
     }
 }
